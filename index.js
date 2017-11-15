7a93cee1f7270a33b41d4850ba21574bb2e845d0
Anthony Bracewell <anthonybracewell.violinist@gmail.com>
2017-11-15 23:20:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,5 +29,5 @@ return function(){
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 funkyFunction()
-console.log(function())
-var theFunk = funkyFunction()
+var theFunk = console.log(function())
+
